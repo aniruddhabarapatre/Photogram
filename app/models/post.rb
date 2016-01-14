892604bf369c1eeb2f7e7ae30b4bac728d5e2ecb
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-14 12:35:03 -0500

    Model for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, dependent: :destroy
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x640" }
