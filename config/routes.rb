f6ca00b9daad83e1ea70aa7c59458c1237565c82
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-24 13:04:43 -0500

    Pretty user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get ':user_name', to: 'profiles#show', as: :profile
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
