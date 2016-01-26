68a4edc707f6cd635d2e8bdb8ca2bd249fa8e565
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-25 22:20:49 -0500

    Set up edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,12 @@
 class ProfilesController < ApplicationController
+  before_action :set_user
+
   def show
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
+
+  private
+    def set_user
+      @user = User.find_by(user_name: params[:user_name])
+    end
 end
