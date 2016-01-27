6eb1736b4e65f856c13c67f6417188bb678a07cb
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-26 21:58:13 -0500

    Form for edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,10 @@ class ProfilesController < ApplicationController
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
+  def edit
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
   private
     def set_user
       @user = User.find_by(user_name: params[:user_name])
